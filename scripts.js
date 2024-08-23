2d4dedf8e476ee2247a83aecdb2d22564fe0bf99
Lucas Souza <henrique.lucaas@outlook.com.br>
2024-08-23 08:05:08 -0400

    Tarefas adicionadas com sucesso e sendo riscadas, mas com um pequeno detalhe a ser ajustado!

--- scripts.js
+++ scripts.js
@@ -7,7 +7,8 @@ let minhaListaDeItens = [];
 // Adiciona uma nova tarefa à lista
 function adicionarNovaTarefa() {
   if (input.value.trim() === '') {
-    alert('Digite uma tarefa!');
+    // Mensagem de erro visual em vez de alert
+    document.querySelector('.error-message').textContent = 'Digite uma tarefa!';
     return;
   }
 
@@ -25,24 +26,19 @@ function mostrarTarefas() {
   let novaLi = '';
 
   minhaListaDeItens.forEach((item, posicao) => {
-    
-    
-            //   ALTERAR IMAGENS E MAIS ALGUMAS COISAS 
-
-     novaLi += `
-       <li class="task ${item.concluida ? 'done' : ''}">
-         <img src="./img/check.png" alt="check-na-tarefa" class="check-icon" data-posicao="${posicao}">
-         <p>${item.tarefa}</p>
-         <img src="./img/trash.png" alt="tarefa-para-o-lixo" class="trash-icon" data-posicao="${posicao}">
-       </li>
-     `;
-
-
+    novaLi += `
+      <li class="task ${item.concluida ? 'done' : ''}">
+        <img src="./img/check.png" alt="check-na-tarefa" class="check-icon" data-posicao="${posicao}">
+        <p>${item.tarefa}</p>
+        <img src="./img/trash.png" alt="tarefa-para-o-lixo" class="trash-icon" data-posicao="${posicao}">
+      </li>
+    `;
   });
 
   listaCompleta.innerHTML = novaLi;
-
   localStorage.setItem('lista', JSON.stringify(minhaListaDeItens));
+
+  configurarEventos(); // Reassocia os eventos após atualizar a lista
 }
 
 // Marca a tarefa como concluída ou não
@@ -60,11 +56,9 @@ function deletarItem(posicao) {
 // Recarrega as tarefas do localStorage
 function recarregarTarefas() {
   const tarefasDoLocalStorage = localStorage.getItem('lista');
-
   if (tarefasDoLocalStorage) {
     minhaListaDeItens = JSON.parse(tarefasDoLocalStorage);
   }
-
   mostrarTarefas();
 }
 
@@ -86,4 +80,3 @@ function configurarEventos() {
 
 recarregarTarefas();
 button.addEventListener('click', adicionarNovaTarefa);
-configurarEventos();
